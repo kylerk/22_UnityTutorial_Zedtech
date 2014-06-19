50d1ea3819fc4241b696e6aa922d08c3a39e4cf4
kylerk <kylerk@gmail.com>
2014-06-19 16:02:48 -0400

    A few little things, nothing important

---
+++ Assets/Scripts/GameBrain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBrain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject prefab=Resources.Load ("Cube") as GameObject;
+		for (int i=0;i<100;i++){
+			GameObject go=Instantiate(prefab) as GameObject;
+			go.transform.position=new Vector3(0,i*5,0);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
