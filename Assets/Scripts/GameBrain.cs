2d7df220ae4430a48049a4e3cc3b91b724277234
kylerk <kylerk@gmail.com>
2014-07-03 16:37:56 -0400

    New Git Ignore.

--- Assets/Scripts/GameBrain.cs
+++ Assets/Scripts/GameBrain.cs
@@ -5,12 +5,14 @@ public class GameBrain : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		{
 		GameObject prefab=Resources.Load ("Cube") as GameObject;
 		for (int i=0;i<100;i++){
 			GameObject go=Instantiate(prefab) as GameObject;
 			go.transform.position=new Vector3(0,i*5,0);
 		}
-
+		}
 	}
 	
 	// Update is called once per frame
