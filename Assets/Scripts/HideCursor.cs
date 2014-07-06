84420a04f027ce8c654df4b5a09ad44ab6ca4465
kylerk <kylerk@gmail.com>
2014-07-06 18:02:41 -0400

    Working out more problems

--- Assets/Scripts/HideCursor.cs
+++ Assets/Scripts/HideCursor.cs
@@ -4,21 +4,21 @@ using System.Collections;
 public class HideCursor : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start ()
-	{
-		Screen.showCursor = false;
-		Screen.lockCursor = true;
+		// Use this for initialization
+		void Start ()
+		{
+				Screen.showCursor = false;
+				Screen.lockCursor = true;
 	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			Screen.showCursor = !Screen.showCursor;
-			Screen.lockCursor = !Screen.lockCursor;
 		}
-	}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+				if (Input.GetKeyDown (KeyCode.Escape)) {
+						Screen.showCursor = !Screen.showCursor;
+						Screen.lockCursor = !Screen.lockCursor;
+				}
+		}
 }
