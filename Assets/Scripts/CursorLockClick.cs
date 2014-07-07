3909f97324412d53ce99a6b0ec2e120d529aadf0
kylerk <kylerk@gmail.com>
2014-07-07 13:22:56 -0400

    New Simple click based cursor lock.  Doesn't seem to get screwed up like the alternate versions that I was trying to use.

---
+++ Assets/Scripts/CursorLockClick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorLockClick : MonoBehaviour
+{	
+
+
+
+
+	void OnMouseDown ()
+	{
+		Screen.lockCursor = true;
+		guiTexture.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Screen.lockCursor = false;
+			guiTexture.enabled = true;
+		}
+
+		
+
+
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/JetPack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JetPack : MonoBehaviour
+{
+
+	CharacterMotor cm;
+	CharacterController cc;
+
+	// Use this for initialization
+	void Start ()
+	{
+		cm = (CharacterMotor)GetComponent<CharacterMotor> ();
+		cc = (CharacterController)GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			Vector3 velocity = new Vector3 (cc.velocity.x, 10, cc.velocity.z);
+			cm.SetVelocity (velocity);
+		}
+
+	}
+}
