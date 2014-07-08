2406a979866ac9b65bd21fcf2ce5c063ebd9b689
kylerk <kylerk@gmail.com>
2014-07-08 17:53:45 -0400

    Another light grid thing.  Nothing too complicated.  Used the bounds of an object though.

---
+++ Assets/Scripts/PlaneGlow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneGlow : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject prefab = Resources.Load ("markerlight") as GameObject;
+		int stepSize = 5;
+		for (int x=0; x<renderer.bounds.size.x; x+=stepSize) {
+			for (int z=0; z<renderer.bounds.size.z; z+=stepSize) {
+				GameObject go = Instantiate (prefab) as GameObject;
+				go.transform.position = new Vector3 (x - renderer.bounds.size.x / 2, 0, z - renderer.bounds.size.z / 2) + transform.position;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
