1813fc44ba164795a43d70b867081a8f8426eea7
kylerk <kylerk@gmail.com>
2014-07-08 13:28:31 -0400

    Added Exploding Particles

---
+++ Assets/Scripts/ExplodingProjectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodingProjectile : MonoBehaviour
+{
+
+
+	void Start ()
+	{
+
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		Destroy (gameObject);
+		GameObject prefab = Resources.Load ("Fireworks") as GameObject;
+		GameObject fireworks = Instantiate (prefab) as GameObject;
+		fireworks.transform.position = transform.position;
+		Destroy (gameObject);
+
+		Destroy (fireworks, 5);
+
+	}
+
+
+}
