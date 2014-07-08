310044c1f09dc62df10d45cbca882554e349349d
kylerk <kylerk@gmail.com>
2014-07-08 13:19:08 -0400

    Add a movement Checkpoint system

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour
+{
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.gameObject.name == "player") {
+			GameVariables.checkpoint = transform.position;
+		}
+	}
+}
---
+++ Assets/Scripts/FallDetect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallDetect : MonoBehaviour
+{
+
+
+	void Start ()
+	{
+	
+		GameVariables.checkpoint = new Vector3 (0, 7, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (transform.position.y < -10) {
+			transform.position = GameVariables.checkpoint;
+
+		
+		}
+	}
+}
