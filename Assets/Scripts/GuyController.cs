a74a2cd9443f1775f5b907959ef7e22d7923edcb
kylerk <kylerk@gmail.com>
2014-07-09 14:58:40 -0400

    Created a blend tree and jump animation for the sprite guy.

--- Assets/Scripts/GuyController.cs
+++ Assets/Scripts/GuyController.cs
@@ -8,16 +8,36 @@ public class GuyController : MonoBehaviour
 	bool facingRight = true;
 
 	Animator anim;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
 	// Use this for initialization
 	void Start ()
 	{
 		anim = GetComponent<Animator> ();
+
+	}
+
+	void Update ()
+	{
+
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool ("ground", false);
+			rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+		}
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-	
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("ground", grounded);
+
+		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
 		float move = Input.GetAxis ("Horizontal");
 
 		anim.SetFloat ("speed", Mathf.Abs (move));
