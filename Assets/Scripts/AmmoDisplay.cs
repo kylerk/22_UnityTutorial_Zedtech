e80fa7bf91be96a3e952a33b90ea53fed99797a2
kylerk <kylerk@gmail.com>
2014-07-10 19:03:54 -0400

    Ammunition Indicator

---
+++ Assets/Scripts/AmmoDisplay.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoDisplay : MonoBehaviour
+{
+
+	Rect rect;
+	Texture texture;
+	float textureSize = 0.0002f;
+	int screenWidth;
+	int screenHeight;
+	Rect newrect;
+	// Use this for initialization
+	void Start ()
+	{   
+
+		texture = Resources.Load ("Textures/Moose") as Texture;
+		screenWidth = Screen.width;
+		screenHeight = Screen.height;	
+
+
+		rect = TextureRect (texture.width, texture.height, textureSize);
+
+
+
+	}
+
+	void Update ()
+	{
+
+		if (screenWidth != Screen.width || screenHeight != Screen.height)
+			rect = TextureRect (texture.width, texture.height, textureSize);
+	}
+	
+	// Update is called once per frame
+	void OnGUI ()
+	{
+		newrect = rect;
+		for (int i = 0; i<GameVariables.ammunition; i++) {
+			//GUI.DrawTexture (rect, texture);	
+
+			newrect.x += rect.width / 2;
+			GUI.DrawTexture (newrect, texture);
+			//Debug.Log (rect);
+
+		}
+	}
+
+	Rect TextureRect (int textureWidth, int textureHeight, float texSize)
+	{
+		return new Rect (Screen.width * 0.1f,
+		                 Screen.height - Screen.width * 0.3f * textureHeight * texSize,
+		                 Screen.width * 0.3f * textureWidth * texSize,
+			         Screen.width * 0.3f * textureHeight * texSize);
+
+
+	}
+
+}
