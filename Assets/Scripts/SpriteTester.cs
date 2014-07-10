c1f418206813389a4bccc5ac9a35d274667d659e
kylerk <kylerk@gmail.com>
2014-07-10 02:41:39 -0400

    Figured out how to programatically change sprites

---
+++ Assets/Scripts/SpriteTester.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteTester : MonoBehaviour
+{
+	Sprite[] sprites;
+	// Use this for initialization
+
+	SpriteRenderer spriteRenderer;
+	int frame = 0;
+	float framerate = 1f / 8f;
+	float time = 0.0f;
+	void Start ()
+	{	
+		sprites = Resources.LoadAll<Sprite> ("SpriteWork/spriteGuy");
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		
+		spriteRenderer.sprite = sprites [frame];
+		time += Time.deltaTime;
+		if (time > framerate) {
+			frame++;
+			time = 0;
+		}
+		
+		if (frame >= sprites.Length)
+			frame = 0;
+	
+	}
+}
