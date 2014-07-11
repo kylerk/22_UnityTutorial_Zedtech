e65db9d7ab8f74f1fc0d3faa64ef5943aff67600
kylerk <kylerk@gmail.com>
2014-07-10 22:43:17 -0400

    TeleporterPads

---
+++ Assets/Scripts/TeleportPad.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportPad : MonoBehaviour
+{
+	public int code;
+	float disableTime = 0f;
+
+	void Update ()
+	{
+		disableTime -= Time.deltaTime;
+
+	}
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.gameObject.name == "player" && disableTime <= 0) {
+
+
+			foreach (TeleportPad tp in FindObjectsOfType<TeleportPad>()) {
+
+				if (tp.code == code && tp != this) {
+					tp.disableTime = 2;
+
+					Vector3 position = tp.gameObject.transform.position;
+					position.y += 8;
+					collider.gameObject.transform.position = position;
+				}
+			}
+		}
+
+
+	}
+}
